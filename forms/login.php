2beeeddd9688b6d6d8ffdc1e510d330331f4862e
John Mark Alvis <100738110+alvisjohnmark@users.noreply.github.com>
2023-05-04 17:45:18 +0800

    put-sticky-btn-shit

--- forms/login.php
+++ forms/login.php
@@ -9,6 +9,7 @@
     <title>Arte crafts</title>
     <link rel="stylesheet" href="../css/style.css">
     <link rel="stylesheet" href="../css/login.css">
+    <link rel="stylesheet" href="../css/index.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" />
 </head>
 
@@ -29,6 +30,7 @@
             </form>
         </div>
     </div>
+    <div class="sticky"><button><a href="../index.php"><i class="fa fa-home"></i></a></button></div>
 </body>
 
 </html>
