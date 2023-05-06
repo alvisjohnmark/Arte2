842a070112794226f206c42891c7862ce3ad6cd8
John Mark Alvis <100738110+alvisjohnmark@users.noreply.github.com>
2023-05-06 18:35:47 +0800

    about

---
+++ pages/about.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>About us</title>
+    <link rel="stylesheet" href="../css/about.css">
+</head>
+
+<body>
+    <header>
+        <div id="header">
+            <div id="bars">
+                <button class="mobile-menu">
+                    <span></span>
+                </button>
+            </div>
+            <div class="brand-name">
+                <a href="../index.php">
+                    <span>Arte</span>
+                    <span>crafts</span>
+                </a>
+            </div>
+        </div>
+    </header>
+    <section>
+        <div class="title-container">
+            <h1>About Us</h1>
+        </div>
+        <div class="description-container">
+            <p>Arte is an iloko word that translates to Art in English. We are a group that aims to support local
+                artists by reselling their handmade products and marketing them nationwide. But we also visualize to
+                broaden the market globally by 2025; enabling our local products becom even more known.</p>
+        </div>
+        <div class="cards">
+            <div class="card">
+                <img src="../assets/images/aloh.jpg" alt="">
+                <div class="name">
+                    <p><b>UI designer</b></p>
+                    <p>Leigh Aloha Badua</p>
+                </div>
+            </div>
+            <div class="card">
+                <img src="../assets/images/jona.jpg" alt="">
+                <div class="name">
+                    <p><b>Senior Developer</b></p>
+                    <p>Jonathan Apostotl</p>
+                </div>
+            </div>
+            <div class="card">
+                <img src="../assets/images/jm.jpg" alt="">
+                <div class="name">
+                    <p><b>Junior Developer</b></p>
+                    <p>John Mark Alvis</p>
+                </div>
+            </div>
+            <div class="card">
+                <img src="../assets/images/earnest.jpg" alt="">
+                <div class="name">
+                    <p><b>Project Manager</b></p>
+                    <p>Earnest Andrei Orcino</p>
+                </div>
+            </div>
+        </div>
+    </section>
+</body>
+
+</html>
